894af4e9c45e5810984d934dfe33a3a9e7e04bfb
Yongjanes <rohit.pali2512@gmail.com>
2025-09-18 06:26:05 +0530

    write user model and card model in the user model added jwt and bcrypt

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,108 @@
+import mongoose from "mongoose"
+
+const cardSchema = mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true
+        },
+        url: {
+            type: String,
+        },
+        icon: {
+            type: String,
+        },
+        order: {
+            type: Number,
+            default: 0
+        },
+        cardType: { 
+            type: String,
+            enum: ["social", "video", "product", "image", "custom"]
+        },
+        style: {
+            background: {
+                type: String
+            },
+            textColor: {
+                type: String
+            }
+        },
+        meta: {
+            type: mongoose.Schema.Types.Mixed,
+            default: {}
+        }
+    }, 
+    {
+        timestamps: true
+    }
+)
+
+const userSchema = mongoose.Schema(
+    {
+        username: { // link name (unique)
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+            index: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true
+        },
+        fullname: { // name that will be displayed in the page
+            type: String,
+            required: true,
+            index: true
+        },
+        avatar: {
+            type: String,
+            default: ""
+        },
+        bio: {
+            type: String
+        },
+        theme: {
+            background: {
+                type: String,
+            },
+            textColor: {
+                type: String
+            },
+            primary: {
+                type: String
+            },
+            secondary: {
+                type: String
+            }, 
+            accent: {
+                type: String
+            },
+            font: {
+                type: String
+            }
+        },
+        password: {
+            type: String,
+            required: [true, "Passwrod is required!"],
+        },
+        cards: [
+            cardSchema
+        ],
+        refreshToken: {
+            type: String
+        }
+    }, 
+    {
+        timestamps: true
+    }
+)
+
+// make functions required for user model
+
+
+export const User = mongoose.model("User", userSchema)
